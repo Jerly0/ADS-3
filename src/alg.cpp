f6ebe680785a8818e1970db6b735d8a907ef227c
Jerly0 <87529148+Jerly0@users.noreply.github.com>
2022-03-13 13:39:50 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,21 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+  *a = *arr;
+  s = size;
+  v = value
+  bool bs(int *arr, int size, int number) {
+    int i = 0, j = size - 1; 
+    while (i < j) {
+        int mid = i + (j - i) / 2;
+        if (arr[mid] == number)
+            return number;
+        else if (arr[mid] > number)
+            j = mid;
+        else
+            i = mid + 1;
+    }
+    return 0;
+}
+  bs(*arr, s, v);
 }
