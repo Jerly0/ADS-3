7f871c20387c1efe0abf4076925928d937e72ff2
Jerly0 <87529148+Jerly0@users.noreply.github.com>
2022-03-13 13:23:50 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,21 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+  a[] = *arr;
+  s = size;
+  v = valeu;
+  bool binSearch(int arr[], int size, int value) {
+    int i = 0, j = size - 1;
+    while(i < j) {
+        int mid = i + (j - i) / 2;
+        if(arr[mid] == value)
+            return value;
+        else if(arr[mid] > value)
+            j = mid;
+        else
+            i = mid + 1;
+    }
+    return 0;
+}
+  binSearch(a[], s, v);
 }
